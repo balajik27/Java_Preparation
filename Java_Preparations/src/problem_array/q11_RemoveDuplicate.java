5487bd153853ae5a1d6ec5864b2acca8f85fbd03
balajik27 <balajik272003@gmail.com>
2023-09-05 21:41:56 +0530

    Day4

--- Java_Preparations/src/problem_array/q11_RemoveDuplicate.java
+++ Java_Preparations/src/problem_array/q11_RemoveDuplicate.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class q11_RemoveDuplicate {
 	static int removeDuplicate(int[] array) {
 		
-		HashSet<Integer> hs = new HashSet<>();
+		HashSet<Integer> hs = new HashSet<>(); // this gives values in unsorted manner
 		
 		for(int n:array) {
 			hs.add(n);
@@ -29,8 +29,8 @@ public class q11_RemoveDuplicate {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int arr[] = {4,3,3,2,6,7,7,1};
-		int k = removeDuplicate(arr);
-		k = Removeduplicate(arr);
+//		int k = removeDuplicate(arr);
+		int k = Removeduplicate(arr);
 		for(int i=0;i<k;i++) {
 			System.out.println(arr[i]);
 		}
